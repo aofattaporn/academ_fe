d56700362819131891562ca4475c0e7026e73ec7
aofattaporn <aofattapon321@gmail.com>
2024-01-10 17:25:05 +0700

    set-up: solve msw warning image defualt

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,12 @@
 import { authMock } from "./authMock";
+import setup from "./setUpWorker";
 
 // ingeneral case handler
 export const handlers = [
+  setup.unhandledRequestSVGHandler,
+  setup.unhandledRequesPNGtHandler,
+  setup.unhandledPostRequestHandler,
+
   authMock.signIn_success,
   authMock.signUp_success,
   authMock.signIn_success,
