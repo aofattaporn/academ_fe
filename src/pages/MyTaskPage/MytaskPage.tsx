c3dd07a3aa9a560769ce08d3c006140f51b6249e
thunnuht <77015864+thunyathep@users.noreply.github.com>
2024-01-25 15:58:57 +0700

    commit : change device

--- src/pages/MyTaskPage/MytaskPage.tsx
+++ src/pages/MyTaskPage/MytaskPage.tsx
@@ -1,7 +1,10 @@
 import Accordion from "@mui/material/Accordion";
-import AccordionDetails from "@mui/material/AccordionDetails";
-// import AccordionGroup from "@mui/material/AccordionGroup";
+import AccordionActions from "@mui/material/AccordionActions";
 import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Button from "@mui/material/Button";
+import CustomizedAccordions from "./component/accordion";
 
 const MytaskPage = () => {
   return (
@@ -19,10 +22,57 @@ const MytaskPage = () => {
           <p className="text-black font-bold text-xl">Jit:D</p>
         </div>
       </div> */}
-      <Accordion className="m-4">
-        <AccordionSummary>Jit:D</AccordionSummary>
-        <AccordionDetails>Content</AccordionDetails>
-      </Accordion>
+      <div className="m-4">
+        <Accordion className="mb-4">
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            <div className="flex align-middl items-center gap-4 p-2 cursor-pointer">
+              <div className="bg-primary w-16 h-16 rounded-md flex justify-center items-center text-center shadow-md">
+                <p className="text-white text-center font-bold text-2xl">J</p>
+              </div>
+              <div>
+                <p className="font-bold text-3xl">Jit:D</p>
+              </div>
+            </div>
+          </AccordionSummary>
+          <AccordionDetails>
+            <CustomizedAccordions />
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2-content"
+            id="panel2-header"
+          >
+            Accordion 2
+          </AccordionSummary>
+          <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </AccordionDetails>
+        </Accordion>
+        <Accordion defaultExpanded>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel3-content"
+            id="panel3-header"
+          >
+            Accordion Actions
+          </AccordionSummary>
+          <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </AccordionDetails>
+          <AccordionActions>
+            <Button>Cancel</Button>
+            <Button>Agree</Button>
+          </AccordionActions>
+        </Accordion>
+      </div>
     </div>
   );
 };
