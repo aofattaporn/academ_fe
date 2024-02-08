bdd2f935663a3dffe52a10f8c49d56c4107b3650
aofattaporn <aofattapon321@gmail.com>
2024-02-08 17:31:13 +0700

    side-bar: extract hook

--- src/layouts/HomeLayout/SideBar/SideBar.tsx
+++ src/layouts/HomeLayout/SideBar/SideBar.tsx
@@ -12,34 +12,17 @@ import FolderIcon from "@mui/icons-material/Folder";
 import NavigateItem from "./NavigateItem/NavigateItem";
 import AcademTitle from "./AcademTitle/AcademTitle";
 import ToggleItem from "./ToggleItem/ToggleItem";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../../../stores/store";
-import { useQuery } from "react-query";
-import projectApi from "../../../libs/projectApi";
-import {
-  saveOnError,
-  saveProjects,
-} from "../../../stores/projectSlice/projectSlice";
+import useAllMyProjects from "../../../hooks/projectHook/useAllMyProjects";
 
 const SideBar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const dispatch = useDispatch();
-  const projects = useSelector((state: RootState) => state.projects.projects);
-
   const {
-    isLoading: projectIsLoading,
-    isSuccess: projectIsSuccess,
-    isError: projectIsError,
-    refetch: projectRefetch,
-    data: projectData,
-  } = useQuery("allProjectData", () => projectApi.getAllProject(projects), {
-    onSuccess: (data) => {
-      dispatch(saveProjects(data));
-    },
-    onError: () => {
-      dispatch(saveOnError(true));
-    },
-  });
+    projectIsLoading,
+    projectIsSuccess,
+    projectIsError,
+    projectData,
+    projectRefetch,
+  } = useAllMyProjects();
 
   // TODO : Get All Class
 
