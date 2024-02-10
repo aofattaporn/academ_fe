b6ac3c610b9bd2304784ad6a88458d052bb18ed1
aofattaporn <aofattapon321@gmail.com>
2024-02-10 15:49:48 +0700

    change query on home-page layout

--- src/layouts/HomeLayout/HomeLayout.tsx
+++ src/layouts/HomeLayout/HomeLayout.tsx
@@ -1,8 +1,21 @@
 import { Outlet } from "react-router-dom";
 import AcademNaveBar from "./Navbar/AcademNavBar";
 import SideBar from "./SideBar/SideBar";
+import { useDispatch } from "react-redux";
+import { useQuery } from "react-query";
+import userApi from "../../libs/userApi";
+import { saveUser } from "../../stores/userSlice/userSlice";
 
 const HomeLayout = () => {
+  const dispatch = useDispatch();
+
+  const { data: userData, isSuccess } = useQuery(
+    "userData",
+    userApi.getUserApi
+  );
+
+  if (isSuccess) dispatch(saveUser(userData));
+
   return (
     <div className="flex">
       <SideBar />
--- src/layouts/PrivateRoute.tsx
+++ src/layouts/PrivateRoute.tsx
@@ -1,23 +1,11 @@
 import { useAuth } from "./AuthProvider";
 import HomeLayout from "./HomeLayout/HomeLayout";
-import AuthLayout from "./AuthLayout";
-import { useQuery } from "react-query";
-import userApi from "../libs/userApi";
-import { saveUser } from "../stores/userSlice/userSlice";
-import { useDispatch } from "react-redux";
+import { Navigate } from "react-router-dom";
 
 const PrivateRoute = () => {
   const { user } = useAuth();
-  const dispatch = useDispatch();
 
-  const { data: userData, isSuccess } = useQuery(
-    "userData",
-    userApi.getUserApi
-  );
-
-  if (isSuccess) dispatch(saveUser(userData));
-
-  return user?.emailVerified ? <HomeLayout /> : <AuthLayout />;
+  return user?.emailVerified ? <HomeLayout /> : <Navigate to={"/sign-in"} />;
 };
 
 export default PrivateRoute;
