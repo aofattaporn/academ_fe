9d958dd72e28b2f88b85e73ae06eda9b3cbeff94
aofattaporn <aofattapon321@gmail.com>
2024-03-19 19:53:33 +0700

    list: add mock case on create tasks failed

--- src/mocks/tasksMock.ts
+++ src/mocks/tasksMock.ts
@@ -1,14 +1,69 @@
 import { HttpResponse, delay, http } from "msw";
 import { Tasks } from "../types/MyTasksType";
 import {
+  RESPONSE_BUSSINESS_ERROR,
   RESPONSE_OK,
   RESPONSE_TECHNICAL_ERROR,
   ResponseCustom,
+  STATUS_CODE_1899,
   STATUS_CODE_1999,
 } from "../types/GenericType";
 import moment from "moment";
 
-// get-project-api
+// TODO: MOCK DATA ON TASKS API
+
+const MOCK_TASKS: Tasks[] = [
+  {
+    tasksId: "123456789",
+    tasksName: "Complete Report222",
+    processId: "1",
+    assignee: "John Doe",
+    startDate: moment("2024-03-06"),
+    dueDate: moment("2024-03-10"),
+  },
+  {
+    tasksId: "987654321",
+    tasksName: "Review Presentation",
+    processId: "1",
+    assignee: "Jane Smith",
+    startDate: moment("2024-03-08"),
+    dueDate: moment("2024-03-12"),
+  },
+  {
+    tasksId: "987654325",
+    tasksName: "Review Presentation",
+    processId: "1",
+    assignee: "Jane Smith",
+    startDate: moment("2024-03-08"),
+    dueDate: moment("2024-03-12"),
+  },
+  {
+    tasksId: "987654215",
+    tasksName: "Review Testcase",
+    processId: "2",
+    assignee: "Jane Smith",
+    startDate: moment("2024-03-08"),
+    dueDate: moment("2024-03-12"),
+  },
+  {
+    tasksId: "987654324",
+    tasksName: "Testing",
+    processId: "2",
+    assignee: "Jane Smith",
+    startDate: moment("2024-03-08"),
+    dueDate: moment("2024-03-12"),
+  },
+  {
+    tasksId: "9876543777",
+    tasksName: "New Testing",
+    processId: "1",
+    assignee: "",
+    startDate: moment("2024-03-08"),
+    dueDate: moment("2024-03-12"),
+  },
+];
+
+// TODO: MOCK RESPONSE TASKS API
 const getAllTasksByProjectId = http.get(
   "api/v1/tasks/projects/:projectId",
   () => {
@@ -16,48 +71,7 @@ const getAllTasksByProjectId = http.get(
       status: 200,
       message: RESPONSE_OK,
       description: "Success",
-      data: [
-        {
-          tasksId: "123456789",
-          tasksName: "Complete Report222",
-          processId: "1",
-          assignee: "John Doe",
-          startDate: moment("2024-03-06"),
-          dueDate: moment("2024-03-10"),
-        },
-        {
-          tasksId: "987654321",
-          tasksName: "Review Presentation",
-          processId: "1",
-          assignee: "Jane Smith",
-          startDate: moment("2024-03-08"),
-          dueDate: moment("2024-03-12"),
-        },
-        {
-          tasksId: "987654325",
-          tasksName: "Review Presentation",
-          processId: "1",
-          assignee: "Jane Smith",
-          startDate: moment("2024-03-08"),
-          dueDate: moment("2024-03-12"),
-        },
-        {
-          tasksId: "987654215",
-          tasksName: "Review Testcase",
-          processId: "2",
-          assignee: "Jane Smith",
-          startDate: moment("2024-03-08"),
-          dueDate: moment("2024-03-12"),
-        },
-        {
-          tasksId: "987654324",
-          tasksName: "Testing",
-          processId: "2",
-          assignee: "Jane Smith",
-          startDate: moment("2024-03-08"),
-          dueDate: moment("2024-03-12"),
-        },
-      ],
+      data: MOCK_TASKS,
     };
 
     return HttpResponse.json(mockRes, { status: 200 });
@@ -88,46 +102,7 @@ const createTasks = http.post("api/v1/tasks", () => {
     message: RESPONSE_OK,
     description: "Success",
     data: [
-      {
-        tasksId: "123456789",
-        tasksName: "Complete Report222",
-        processId: "1",
-        assignee: "John Doe",
-        startDate: moment("2024-03-06"),
-        dueDate: moment("2024-03-10"),
-      },
-      {
-        tasksId: "987654321",
-        tasksName: "Review Presentation",
-        processId: "1",
-        assignee: "Jane Smith",
-        startDate: moment("2024-03-08"),
-        dueDate: moment("2024-03-12"),
-      },
-      {
-        tasksId: "987654325",
-        tasksName: "Review Presentation",
-        processId: "1",
-        assignee: "Jane Smith",
-        startDate: moment("2024-03-08"),
-        dueDate: moment("2024-03-12"),
-      },
-      {
-        tasksId: "987654215",
-        tasksName: "Review Testcase",
-        processId: "2",
-        assignee: "Jane Smith",
-        startDate: moment("2024-03-08"),
-        dueDate: moment("2024-03-12"),
-      },
-      {
-        tasksId: "987654324",
-        tasksName: "Testing",
-        processId: "2",
-        assignee: "Jane Smith",
-        startDate: moment("2024-03-08"),
-        dueDate: moment("2024-03-12"),
-      },
+      ...MOCK_TASKS,
       {
         tasksId: "9876543777",
         tasksName: "New Testing",
@@ -139,6 +114,19 @@ const createTasks = http.post("api/v1/tasks", () => {
     ],
   };
 
+  return HttpResponse.json(mockRes, { status: 200 });
+});
+
+const createTasksFailedInternalError = http.post("api/v1/tasks", () => {
+  () => HttpResponse.error();
+});
+
+const createTasksFaildInvalidFeild = http.post("api/v1/tasks", () => {
+  const mockRes: ResponseCustom<null> = {
+    status: STATUS_CODE_1899,
+    message: RESPONSE_BUSSINESS_ERROR,
+    description: "CANT TO GET PROJECT ID",
+  };
   return HttpResponse.json(mockRes, { status: 200 });
 });
 
@@ -201,17 +189,14 @@ const deleteTasksFailedInternalError = http.delete(
   () => HttpResponse.error()
 );
 
-const deleteTasksFailedNotFoundId = http.delete(
-  "api/v1/tasks/:tasksId",
-  async () => {
-    const mockRes: ResponseCustom<null> = {
-      status: STATUS_CODE_1999,
-      message: RESPONSE_TECHNICAL_ERROR,
-      description: "NOT FOUND TASK ID",
-    };
-    return HttpResponse.json(mockRes, { status: 200 });
-  }
-);
+const deleteTasksFailedNotFoundId = http.delete("api/v1/tasks/:tasksId", () => {
+  const mockRes: ResponseCustom<null> = {
+    status: STATUS_CODE_1999,
+    message: RESPONSE_TECHNICAL_ERROR,
+    description: "NOT FOUND TASK ID",
+  };
+  return HttpResponse.json(mockRes, { status: 200 });
+});
 
 const changeProcess = http.put(
   "api/v1/tasks/:tasksId/process/:processId",
@@ -285,6 +270,8 @@ export const tasksMock = {
 
   // create-tasks-api-mocking
   createTasks,
+  createTasksFailedInternalError,
+  createTasksFaildInvalidFeild,
 
   // change-process-tasks-api-mocking
   changeProcess,
