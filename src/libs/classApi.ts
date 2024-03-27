aa84c96592d51dcedcc6dffab4edc492dbfe92d1
thunnuht <77015864+thunyathep@users.noreply.github.com>
2024-03-27 09:17:44 +0700

    update : class api

---
+++ src/libs/classApi.ts
@@ -0,0 +1,38 @@
+import { Class, ListClass } from "../types/ClassType";
+import axiosInstance from "./axiosInstance";
+import firebaseApi from "./firebaseApi";
+
+const getAllClass = async (): Promise<ListClass[]> => {
+    try {
+      const token = await firebaseApi.getToken();
+      const response = await axiosInstance.get("/api/v1/class/users/id", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return response.data.data;
+    } catch (error) {
+      console.error("Error fetching class:", error);
+      throw error;
+    }
+  };
+
+const getClass = async (classId: string): Promise<Class> => {
+    try {
+      const token = await firebaseApi.getToken();
+      const response = await axiosInstance.get(`/api/v1/class/${classId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return response.data.data;
+    } catch (error) {
+      console.error("Error fetching class:", error);
+      throw error;
+    }
+  };
+
+
+const classApi = {getAllClass,getClass};
+
+export default classApi;
