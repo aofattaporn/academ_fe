889915a5c43082636a9a5faf0420c33ab361562a
aofattaporn <aofattapon321@gmail.com>
2024-04-30 06:56:18 +0700

    permission: check isDelete on Board

--- src/pages/ProjectPage/Board/BoardItem/BoardItem.tsx
+++ src/pages/ProjectPage/Board/BoardItem/BoardItem.tsx
@@ -3,17 +3,21 @@ import { Tasks } from "../../../../types/MyTasksType";
 import { Avatar } from "@mui/material";
 import Tooltip from "@mui/material/Tooltip";
 import SettingTasksTile from "../../../../components/ListAccordion/ListAccordionItem/SettingTasksTile/SettingTasksTile";
+import { TaskPermission } from "../../../../types/Permission";
 
 type BoardItem = {
   tasks: Tasks;
+  taskPermission: TaskPermission;
 };
 
-const BoardItem = ({ tasks }: BoardItem) => {
+const BoardItem = ({ tasks, taskPermission }: BoardItem) => {
   return (
     <div className="bg-main w-full mt-4 p-4 shadow-3xl rounded-md hover:bg-gray-100 hover:cursor-pointer group">
       <div className="flex justify-between items-center">
         <p>{tasks.tasksName}</p>
-        <SettingTasksTile tasksId={tasks.tasksId} isVisible={false} />
+        {taskPermission.delete ? (
+          <SettingTasksTile tasksId={tasks.tasksId} isVisible={false} />
+        ) : null}
       </div>
 
       <div className="flex justify-between items-center mt-4">
