4d6cddd574e3d4e5802ebf9cf315a658e86a223f
thunnuht <77015864+thunyathep@users.noreply.github.com>
2024-05-06 15:53:08 +0700

    Update : search project

--- src/pages/ProjectPage/ProjectInfo/SettingModal/Search.tsx
+++ src/pages/ProjectPage/ProjectInfo/SettingModal/Search.tsx
@@ -8,13 +8,11 @@ import { json } from "stream/consumers";
 import { ListProject } from "../../../../types/ProjectType";
 import axios from "axios";
 import useAllMyProjects from "../../../../hooks/projectHook/useAllMyProjects";
+import { Link } from "react-router-dom";
 
 const Search = () => {
   const [search, setSearch] = useState("");
-  const {
-    projectData,
-    projectRefetch,
-  } = useAllMyProjects();
+  const { projectData } = useAllMyProjects();
   const handleChange = (value: string) => {
     setSearch(value);
   };
@@ -30,14 +28,36 @@ const Search = () => {
         onChange={(e) => handleChange(e.target.value)}
       />
 
-      <div className="">{projectData?.map((project)=>{
-        return(
-            <li>
-                {project.projectProfile.projectName}
-            </li>
-        );
-      }):null}
-      </div>
+      {projectData
+        ?.filter((project) => {
+          const projectName = project.projectProfile.projectName.toLowerCase();
+          const searchItem = search.toLowerCase();
+          return searchItem === "" || projectName.includes(searchItem);
+        })
+        .map((project) => {
+          return (
+            <Link to={`/projects/${project.projectId}`}>
+              <div
+                key={project.projectId}
+                className="hover:cursor-pointer hover:bg-background-primary_subtle py-2 px-2 mt-2 rounded-md"
+              >
+                <div className="flex gap-4 items-center">
+                  <h1
+                    style={{
+                      backgroundColor: project.projectProfile.avatarColor,
+                    }}
+                    className="text-xl font-bold bg-primary rounded-md  w-12 h-12 flex items-center justify-center text-white"
+                  >
+                    {project.projectProfile.projectName.charAt(0)}
+                  </h1>
+                  <h1 className="text-xl font-bold text-dark">
+                    {project.projectProfile.projectName}
+                  </h1>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
     </div>
   );
 };
