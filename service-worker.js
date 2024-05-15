bdba710ce12a17d46a29a63562b8f8560a3b3dfd
aofattaporn <aofattapon321@gmail.com>
2024-05-15 21:08:25 +0700

    fcm: register service worker

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+if ("serviceWorker" in navigator) {
+  // Register a service worker hosted at the root of the
+  // site using the default scope.
+  navigator.serviceWorker
+    .register(`https://academ-fe.onrender.com/service-worker.js`)
+    .then(
+      (registration) => {
+        console.log("Service worker registration succeeded:", registration);
+      },
+      /*catch*/ (error) => {
+        console.error(`Service worker registration failed: ${error}`);
+      }
+    );
+} else {
+  console.error("Service workers are not supported.");
+}
