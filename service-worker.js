ef3b1ecf9f55758657ced0e11821584104526728
aofattaporn <aofattapon321@gmail.com>
2024-05-16 13:46:51 +0700

    fcm: change service worker file content

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,50 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register(`https://academ-fe.onrender.com/service-worker.js`)
-    .then(
-      (registration) => {
-        console.log("Service worker registration succeeded:", registration);
-      },
-      /*catch*/ (error) => {
-        console.error(`Service worker registration failed: ${error}`);
+// Name of the cache
+const CACHE_NAME = "v1";
+
+// Resources to cache
+const URLS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/image.png",
+];
+
+// Install event - caching resources
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch event - serving cached resources
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Cache hit - return the response from the cache
+      if (response) {
+        return response;
       }
-    );
-} else {
-  console.error("Service workers are not supported.");
-}
+      return fetch(event.request);
+    })
+  );
+});
+
+// Activate event - updating the service worker
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
