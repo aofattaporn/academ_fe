ff48c655b30fef4033574aa4910a600c585dc966
aofattaporn <aofattapon321@gmail.com>
2024-05-16 23:18:14 +0700

    fcm: test fcm

--- src/Firebase.ts
+++ src/Firebase.ts
@@ -22,29 +22,36 @@ const auth = getAuth(app);
 export const messaging = getMessaging(app);
 
 export const requestNotificationPermission = async () => {
-  try {
-    const permission = await Notification.requestPermission();
-    if (permission === "granted") {
-      console.log("Notification permission granted.");
+  // try {
+  const permission = await Notification.requestPermission();
 
-      const token = await getToken(messaging, {
-        vapidKey:
-          "BIB7rdk1yUCifFxK7PdTOGKe37fcoM3_k3KeRtC_ZzOf6nWTbQPJ3mdubyLvKTs6FA6R4bL3pl7fYakXg_rv8H0",
-      });
-      console.log("FCM Token:", token);
-      return token;
-    } else if (permission === "denied") {
-      console.log("Notification permission denied.");
-      return "ea_GlmGV0GyawwlSd1mp-m:APA91bFR1ZEQZcCWhlLZ5H9DmECnJ_Oz-grgmjzwgsA2dqdpPVN5jhUqwMtEHcmpeON2UdMY_4ruQ2Z3H_OFdvI_bMtKMBYFRY-QFiXSfGfpQc9Blze66VvfoArQDSf4IJooSQkG42jO";
-    }
-  } catch (error) {
-    console.error("Error requesting notification permission:", error);
-    console.log(
-      "FCM Token:",
-      "ea_GlmGV0GyawwlSd1mp-m:APA91bFR1ZEQZcCWhlLZ5H9DmECnJ_Oz-grgmjzwgsA2dqdpPVN5jhUqwMtEHcmpeON2UdMY_4ruQ2Z3H_OFdvI_bMtKMBYFRY-QFiXSfGfpQc9Blze66VvfoArQDSf4IJooSQkG42jO"
-    );
+  if (permission === "granted") {
     return "ea_GlmGV0GyawwlSd1mp-m:APA91bFR1ZEQZcCWhlLZ5H9DmECnJ_Oz-grgmjzwgsA2dqdpPVN5jhUqwMtEHcmpeON2UdMY_4ruQ2Z3H_OFdvI_bMtKMBYFRY-QFiXSfGfpQc9Blze66VvfoArQDSf4IJooSQkG42jO";
   }
+
+  return "ea_GlmGV0GyawwlSd1mp-m:APA91bFR1ZEQZcCWhlLZ5H9DmECnJ_Oz-grgmjzwgsA2dqdpPVN5jhUqwMtEHcmpeON2UdMY_4ruQ2Z3H_OFdvI_bMtKMBYFRY-QFiXSfGfpQc9Blze66VvfoArQDSf4IJooSQkG42jO";
+
+  //   if (permission === "granted") {
+  //     console.log("Notification permission granted.");
+
+  //     const token = await getToken(messaging, {
+  //       vapidKey:
+  //         "BIB7rdk1yUCifFxK7PdTOGKe37fcoM3_k3KeRtC_ZzOf6nWTbQPJ3mdubyLvKTs6FA6R4bL3pl7fYakXg_rv8H0",
+  //     });
+  //     console.log("FCM Token:", token);
+  //     return token;
+  //   } else if (permission === "denied") {
+  //     console.log("Notification permission denied.");
+  //     return "ea_GlmGV0GyawwlSd1mp-m:APA91bFR1ZEQZcCWhlLZ5H9DmECnJ_Oz-grgmjzwgsA2dqdpPVN5jhUqwMtEHcmpeON2UdMY_4ruQ2Z3H_OFdvI_bMtKMBYFRY-QFiXSfGfpQc9Blze66VvfoArQDSf4IJooSQkG42jO";
+  //   }
+  // } catch (error) {
+  //   console.error("Error requesting notification permission:", error);
+  //   console.log(
+  //     "FCM Token:",
+  //     "ea_GlmGV0GyawwlSd1mp-m:APA91bFR1ZEQZcCWhlLZ5H9DmECnJ_Oz-grgmjzwgsA2dqdpPVN5jhUqwMtEHcmpeON2UdMY_4ruQ2Z3H_OFdvI_bMtKMBYFRY-QFiXSfGfpQc9Blze66VvfoArQDSf4IJooSQkG42jO"
+  //   );
+  //   return "ea_GlmGV0GyawwlSd1mp-m:APA91bFR1ZEQZcCWhlLZ5H9DmECnJ_Oz-grgmjzwgsA2dqdpPVN5jhUqwMtEHcmpeON2UdMY_4ruQ2Z3H_OFdvI_bMtKMBYFRY-QFiXSfGfpQc9Blze66VvfoArQDSf4IJooSQkG42jO";
+  // }
 };
 
 export const onMessageListener = () =>
