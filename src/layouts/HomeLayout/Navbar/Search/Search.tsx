8e0966a72aa2a8257c532f49066aa859b5fcacd0
aofattaporn <aofattapon321@gmail.com>
2024-05-25 17:59:02 +0700

    search: search by class

--- src/layouts/HomeLayout/Navbar/Search/Search.tsx
+++ src/layouts/HomeLayout/Navbar/Search/Search.tsx
@@ -20,7 +20,7 @@ const Search = () => {
   };
 
   return (
-    <div className="">
+    <div className="h-72 overflow-y-scroll">
       <TextField
         type="text"
         placeholder="Search"
@@ -30,52 +30,83 @@ const Search = () => {
         onChange={(e) => handleChange(e.target.value)}
       />
 
-      {projectData
-        ?.filter((project) => {
+      <div className="overflow-y-scroll mt-8">
+        {projectData?.filter((project) => {
           const projectName = project.projectProfile.projectName.toLowerCase();
           const searchItem = search.toLowerCase();
-          return searchItem === "" || projectName.includes(searchItem);
-        })
-        .map((project) => {
-          return (
-            <div
-              onClick={() => {
-                dispatch(closeModal());
-                navigate(`/projects/${project.projectId}`);
-              }}
-            >
-              <div
-                key={project.projectId}
-                className="hover:cursor-pointer hover:bg-main py-2 px-2 mt-2 rounded-md flex justify-between"
-              >
-                <div className="flex gap-4">
-                  <AvatarProject
-                    isLoading={projectIsLoading}
-                    size={Size.small}
-                    projectName={project.projectProfile.projectName}
-                    color={project.projectProfile.avatarColor}
-                  />
-                  <div>
-                    <h1 className="text-md font-semibold text-dark">
-                      {project.projectProfile.projectName}
-                    </h1>
-                    <p className=" text-gray-600 text-sm">
-                      {project.members.length} members
-                    </p>
-                  </div>
-                  <div>
-                    <ClassNameItem className={project.className} />
-                  </div>
-                </div>
 
-                <ProjectAlertItem
-                  projectEndDate={project.projectEndDate}
-                  isArchive={project.isArchive}
-                />
-              </div>
-            </div>
+          const className = project.className.toLowerCase();
+          const cSearchItem = search.toLowerCase();
+
+          return (
+            searchItem === "" ||
+            projectName.includes(searchItem) ||
+            className.includes(cSearchItem)
           );
-        })}
+        }).length === 0 ? (
+          <div>
+            <h1 className="text-center text-gray-600 font-bold">
+              No projects found
+            </h1>
+
+            <p className="text-center text-gray-600">
+              Please try a different search term.
+            </p>
+          </div>
+        ) : (
+          projectData
+            ?.filter((project) => {
+              const projectName =
+                project.projectProfile.projectName.toLowerCase();
+              const searchItem = search.toLowerCase();
+
+              const className = project.className.toLowerCase();
+              const cSearchItem = search.toLowerCase();
+
+              return (
+                searchItem === "" ||
+                projectName.includes(searchItem) ||
+                className.includes(cSearchItem)
+              );
+            })
+            .map((project) => {
+              return (
+                <div
+                  key={project.projectId}
+                  onClick={() => {
+                    dispatch(closeModal());
+                    navigate(`/projects/${project.projectId}`);
+                  }}
+                  className="hover:cursor-pointer hover:bg-main py-2 px-2 mt-2 rounded-md flex justify-between"
+                >
+                  <div className="flex gap-4">
+                    <AvatarProject
+                      isLoading={projectIsLoading}
+                      size={Size.small}
+                      projectName={project.projectProfile.projectName}
+                      color={project.projectProfile.avatarColor}
+                    />
+                    <div>
+                      <h1 className="text-md font-semibold text-dark">
+                        {project.projectProfile.projectName}
+                      </h1>
+                      <p className=" text-gray-600 text-sm">
+                        {project.members.length} members
+                      </p>
+                    </div>
+                    <div>
+                      <ClassNameItem className={project.className} />
+                    </div>
+                  </div>
+                  <ProjectAlertItem
+                    projectEndDate={project.projectEndDate}
+                    isArchive={project.isArchive}
+                  />
+                </div>
+              );
+            })
+        )}
+      </div>
     </div>
   );
 };
